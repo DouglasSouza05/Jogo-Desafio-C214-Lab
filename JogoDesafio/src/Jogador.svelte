b089f598d613651935ebd92715c0a64dfad73249
Douglas Brandão de Souza <douglasbsouza01@gmail.com>
2023-09-07 17:33:42 -0300

    Começando organização do codigo aplicando HTML e CSS

--- JogoDesafio/src/Jogador.svelte
+++ JogoDesafio/src/Jogador.svelte
@@ -3,5 +3,35 @@
 </script>
 
 <div>
-    <button on:click={gol}>Marcar Gol</button>
+    <main id="main-container">
+        <section id="soccer-game">
+            <form on:submit={gol}>
+                <div class="input-box">
+                    <label for="name"> Digite seu nome </label>
+                    <div id="input-field">
+                        <i class="fa-solid fa-file-signature" />
+                        <input
+                            type="text"
+                            id="name"
+                            placeholder="Ex: Douglas"
+                            required
+                        />
+                    </div>
+                </div>
+                <!-- <button on:click={gol}>Marcar Gol</button> -->
+                <button type="submit">Marcar Gol</button>
+            </form>
+
+            <div id="infos" class="hidden">
+                <h1>Exibindo informações dos seus Gols!</h1>
+                <div id="result">
+                    <span>Seus Gols:</span>
+                    <span id="gols" />
+                </div>
+                <div id="description">
+                    <span />
+                </div>
+            </div>
+        </section>
+    </main>
 </div>
--- JogoDesafio/src/Jogo.svelte
+++ JogoDesafio/src/Jogo.svelte
@@ -3,11 +3,19 @@
 	// Importando dependência externa Howler, para permitir som no navegador
 	import { Howl, Howler } from "howler";
 
+	const form = document.getElementById("form");
+	// form.addEventListener("submit", function (event) {
+	// 	event.preventDefault();
+	// });
+
 	function marcaGol() {
 		console.log("Marcou 1 Gol!");
 	}
 
-	function handleGol() {
+	function handleGol(event) {
+		// Impede o comportamento padrão do formulário
+		event.preventDefault();
+
 		const sound = new Howl({
 			src: ["sounds/tetra.mp3"],
 		});
@@ -15,10 +23,11 @@
 		marcaGol();
 		sound.play();
 		console.log("Playing tetra.mp3");
+
+		document.getElementById("infos").classList.remove("hidden");
 	}
 </script>
 
 <main>
-	<h1>Exibindo informação quando marca Gol!</h1>
 	<Jogador gol={handleGol} />
 </main>
